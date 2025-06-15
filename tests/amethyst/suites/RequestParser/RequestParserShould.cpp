933bebb15a50ec577ae8e25ca90c7aa4c8a7ea1d
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-15 23:33:32 +0200

    feat: parse_basic_request_line

---
+++ tests/amethyst/suites/RequestParser/RequestParserShould.cpp
@@ -0,0 +1,16 @@
+#include "test/test.hpp"
+#include "test/assert/assert.hpp"
+#include "WebServer/Client/Request/RequestParser/RequestParser.hpp"
+
+TEST(parse_basic_GET_request_line)
+{
+    Request_t request;
+    RequestParser sut;
+
+    SimpleResult result = sut.parseRequestLine(request, "GET / HTTP/1.1");
+
+    ASSERT_TRUE(result.isSuccess());
+    ASSERT_EQUALS(GET, request.method);
+    ASSERT_EQUALS("/", request.target);
+    ASSERT_EQUALS("HTTP/1.1", request.httpVersion);
+}
