ef6b26bdc6f658c11724b34f85551594f821d02a
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-15 23:39:32 +0200

    feat: parse_basic_POST_request_line

--- tests/amethyst/suites/RequestParser/RequestParserShould.cpp
+++ tests/amethyst/suites/RequestParser/RequestParserShould.cpp
@@ -14,3 +14,16 @@ TEST(parse_basic_GET_request_line)
     ASSERT_EQUALS("/", request.target);
     ASSERT_EQUALS("HTTP/1.1", request.httpVersion);
 }
+
+TEST(parse_basic_POST_request_line)
+{
+    Request_t request;
+    RequestParser sut;
+
+    SimpleResult result = sut.parseRequestLine(request, "POST / HTTP/1.1");
+
+    ASSERT_TRUE(result.isSuccess());
+    ASSERT_EQUALS(POST, request.method);
+    ASSERT_EQUALS("/", request.target);
+    ASSERT_EQUALS("HTTP/1.1", request.httpVersion);
+}
