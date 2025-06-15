b3bc52aa0568f306d2bae4ff38761d2f95c20bdf
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-15 16:17:17 +0200

    fix: fix ignored multiple assertions caused by an early return in ASSERT macros

--- tests/amethyst/src/test/assert/assert.hpp
+++ tests/amethyst/src/test/assert/assert.hpp
@@ -6,67 +6,63 @@
 
 #define LOG_IDENTATION "          "
 
-#define ASSERT_TRUE(condition)                                                   \
-    if ((condition) == true)                                                     \
-        return;                                                                  \
-                                                                                 \
-    std::ostringstream oss;                                                      \
-    oss << __FILE__ << ":" << __LINE__ << "\n"                                   \
-        << LOG_IDENTATION << "Reason: ASSERT_TRUE(" << #condition << ") failed"; \
-                                                                                 \
-    throw TestFailure(oss.str());
+static std::ostringstream oss;
 
-#define ASSERT_FALSE(condition)                                                   \
-    if ((condition) == false)                                                     \
-        return;                                                                   \
-                                                                                  \
-    std::ostringstream oss;                                                       \
-    oss << __FILE__ << ":" << __LINE__ << "\n"                                    \
-        << LOG_IDENTATION << "Reason: ASSERT_FALSE(" << #condition << ") failed"; \
-                                                                                  \
-    throw TestFailure(oss.str());
+#define ASSERT_TRUE(condition)                                                       \
+    if ((condition) == false)                                                        \
+    {                                                                                \
+        oss << __FILE__ << ":" << __LINE__ << "\n"                                   \
+            << LOG_IDENTATION << "Reason: ASSERT_TRUE(" << #condition << ") failed"; \
+                                                                                     \
+        throw TestFailure(oss.str());                                                \
+    }
 
-#define ASSERT_EQUALS(expected, actual)                                                               \
-    if ((expected) == (actual))                                                                       \
-        return;                                                                                       \
-                                                                                                      \
-    std::ostringstream oss;                                                                           \
-    oss << __FILE__ << ":" << __LINE__ << "\n"                                                        \
-        << LOG_IDENTATION << "Reason: ASSERT_EQUALS(" << #expected << ", " << #actual << ") failed\n" \
-        << LOG_IDENTATION << "Expected: " << (expected) << "\n"                                       \
-        << LOG_IDENTATION << "Actual: " << (actual);                                                  \
-                                                                                                      \
-    throw TestFailure(oss.str());
+#define ASSERT_FALSE(condition)                                                       \
+    if ((condition) == true)                                                          \
+    {                                                                                 \
+        oss << __FILE__ << ":" << __LINE__ << "\n"                                    \
+            << LOG_IDENTATION << "Reason: ASSERT_FALSE(" << #condition << ") failed"; \
+                                                                                      \
+        throw TestFailure(oss.str());                                                 \
+    }
 
-#define ASSERT_NOT_EQUALS(expected, actual)                                                               \
+#define ASSERT_EQUALS(expected, actual)                                                                   \
     if ((expected) != (actual))                                                                           \
-        return;                                                                                           \
+    {                                                                                                     \
+        oss << __FILE__ << ":" << __LINE__ << "\n"                                                        \
+            << LOG_IDENTATION << "Reason: ASSERT_EQUALS(" << #expected << ", " << #actual << ") failed\n" \
+            << LOG_IDENTATION << "Expected: " << (expected) << "\n"                                       \
+            << LOG_IDENTATION << "Actual: " << (actual);                                                  \
                                                                                                           \
-    std::ostringstream oss;                                                                               \
-    oss << __FILE__ << ":" << __LINE__ << "\n"                                                            \
-        << LOG_IDENTATION << "Reason: ASSERT_NOT_EQUALS(" << #expected << ", " << #actual << ") failed\n" \
-        << LOG_IDENTATION << "Expected: " << (expected) << "\n"                                           \
-        << LOG_IDENTATION << "Actual: " << (actual);                                                      \
-                                                                                                          \
-    throw TestFailure(oss.str());
+        throw TestFailure(oss.str());                                                                     \
+    }
+
+#define ASSERT_NOT_EQUALS(expected, actual)                                                                   \
+    if ((expected) == (actual))                                                                               \
+    {                                                                                                         \
+        oss << __FILE__ << ":" << __LINE__ << "\n"                                                            \
+            << LOG_IDENTATION << "Reason: ASSERT_NOT_EQUALS(" << #expected << ", " << #actual << ") failed\n" \
+            << LOG_IDENTATION << "Expected: " << (expected) << "\n"                                           \
+            << LOG_IDENTATION << "Actual: " << (actual);                                                      \
+                                                                                                              \
+        throw TestFailure(oss.str());                                                                         \
+    }
 
 #define ASSERT_THROWS(SUT, excp)                                                                                               \
-    std::ostringstream oss;                                                                                                    \
                                                                                                                                \
     try                                                                                                                        \
     {                                                                                                                          \
         SUT();                                                                                                                 \
         oss << __FILE__ << ":" << __LINE__ << "\n"                                                                             \
             << LOG_IDENTATION << "Reason: ASSERT_THROWS(" << #SUT << ", " << #excp << ") failed: Didn't throw an exception\n"; \
+        throw TestFailure(oss.str());                                                                                          \
     }                                                                                                                          \
     catch (excp & e)                                                                                                           \
     {                                                                                                                          \
-        return;                                                                                                                \
     }                                                                                                                          \
     catch (const std::exception &e)                                                                                            \
     {                                                                                                                          \
         oss << __FILE__ << ":" << __LINE__ << "\n"                                                                             \
             << LOG_IDENTATION << "Reason: ASSERT_THROWS(" << #SUT << ", " << #excp << ") failed: Threw a wrong exception\n";   \
-    }                                                                                                                          \
-                                                                                                                               \
-    throw TestFailure(oss.str());
+        throw TestFailure(oss.str());                                                                                          \
+    }
