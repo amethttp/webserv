ed1e3d71b5020241f4d34b47daca7562274793cc
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-15 21:51:30 +0200

    refactor: change parseRequestMethod method signature

--- src/WebServer/Client/Request/RequestParser/RequestParser.cpp
+++ src/WebServer/Client/Request/RequestParser/RequestParser.cpp
@@ -2,18 +2,15 @@
 #include "utils/string/string.hpp"
 #include <vector>
 
-static Result<method_t> parseRequestMethod(const std::string &requestMethod)
+static method_t parseRequestMethod(const std::string &requestMethod)
 {
     if (requestMethod == "GET")
-        return Result<method_t>::ok(GET);
+        return GET;
     else if (requestMethod == "POST")
-        return Result<method_t>::ok(POST);
+        return POST;
     else if (requestMethod == "DELETE")
-        return Result<method_t>::ok(DELETE);
-    else if (!requestMethod.empty())
-        return Result<method_t>::fail("501 Not Implemented");
-    else
-        return Result<method_t>::fail("400 Bad Request");
+        return DELETE;
+    return NOT_IMPLEMENTED;
 }
 
 Result<bool> RequestParser::parseRequestLine(Request_t &request, const std::string &requestLine)
@@ -23,12 +20,14 @@ Result<bool> RequestParser::parseRequestLine(Request_t &request, const std::stri
     if (splittedRequestLine.size() != 3)
         return Result<bool>::fail("400 Bad Request");
 
-    Result<method_t> methodResult = parseRequestMethod(splittedRequestLine[0]);
+    if (splittedRequestLine[0].empty())
+        return Result<bool>::fail("400 Bad Request");
+
+    request.method = parseRequestMethod(splittedRequestLine[0]);
     
-    if (methodResult.isFailure())
-        return Result<bool>::fail(methodResult.getError());
-        
-    request.method = methodResult.getValue();
+    if (request.method == NOT_IMPLEMENTED)
+        return Result<bool>::fail("501 Not Implemented");
+
     request.target = splittedRequestLine[1];
     request.httpVersion = splittedRequestLine[2];
 
