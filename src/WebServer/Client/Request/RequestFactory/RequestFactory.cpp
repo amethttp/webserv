d796d9e620f3de84d646a7925a83e65bbf1de89e
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-15 19:39:38 +0200

    refactor: add requestBuffer splitting

--- src/WebServer/Client/Request/RequestFactory/RequestFactory.cpp
+++ src/WebServer/Client/Request/RequestFactory/RequestFactory.cpp
@@ -1,12 +1,17 @@
 #include "RequestFactory.hpp"
+#include "utils/string/string.hpp"
+#include <vector>
 
 Result<RequestInfo_t> RequestFactory::create(const std::string &requestBuffer)
 {
     RequestInfo_t requestInfo;
-    
-    if (requestBuffer.find("GET") == 0)
+
+    std::vector<std::string> splittedRequestBuffer = split(requestBuffer, "\r\n");
+    std::vector<std::string> splittedRequestLine = split(splittedRequestBuffer[0], " ");
+
+    if (splittedRequestLine[0] == "GET")
         requestInfo.request.method = GET;
-    else if (requestBuffer.find("POST") == 0)
+    else if (splittedRequestLine[0] == "POST")
         requestInfo.request.method = POST;
     else
         requestInfo.request.method = DELETE;
