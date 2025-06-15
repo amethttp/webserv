1feed9e07c34b68f1002370e797157bfdac73ec4
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-15 18:32:50 +0200

    chore: remove Request class

--- src/WebServer/Client/Request/Request.hpp
+++ src/WebServer/Client/Request/Request.hpp
@@ -1,60 +1,14 @@
 #pragma once
 
 #include <map>
-#include <vector>
 #include <string>
-#include <ostream>
 #include "utils/http.hpp"
 
-#define MAX_URI_LENGTH 8000
-#define VALID_PROTOCOL "HTTP/1.1"
-#define HEADERS_DELIM "\r\n\r\n"
-#define HEADER_LINE_SEP "\r\n"
-#define REQUEST_LINE_SEP ' '
-#define CONTENT_LENGTH "Content-Length"
-#define TRANSFER_ENCODING "Transfer-Encoding"
-
-class Request
+typedef struct Request_s
 {
-private:
-	static const std::string tchars;
-
-	method_t method_;
-	std::string target_;
-	std::string httpVersion_;
-	std::map<std::string, std::string> headers_;
-	std::string body_;
-	std::string buffer_;
-	bool valid_;
-
-	method_t getHTTPMethod(const std::string &method);
-	bool isTokenChar(const char c);
-	bool isToken(const std::string &string);
-	bool isValidHeaderKey(const std::string &key);
-	bool isValidHeaderValue(const std::string &value);
-	bool isValidHeader(const std::string &key, const std::string &value);
-	bool hasValidHost();
-	bool isValidChunkExtensionValue(const std::string &chunkExtensionValue);
-	bool isValidChunkExtension(const std::string &chunkExtension);
-	bool isValidChunkSize(const std::string &string);
-	size_t getChunkSize(const std::string &string);
-	bool shouldWaitForData(const std::string &str);
-	bool tryParseRequestLine(const std::string &string);
-	bool tryParseHeaders(std::vector<std::string> &headers);
-	bool tryParseFullBody();
-	bool tryParseChunkedBody(std::vector<std::string>::iterator &bodyIt, std::vector<std::string>::iterator &bodyEnd);
-	bool tryParseBody(std::vector<std::string> &headers);
-	
-public:
-	Request();
-	~Request();
-
-	std::string getBuffer();
-	void clear();
-	void appendBuffer(char *buffer);
-
-	bool hasFullHeaders();
-	bool tryParseFromBuffer();
-
-	friend std::ostream &operator<<(std::ostream &stream, Request &request);
-};
+	method_t method;
+	std::string target;
+	std::string httpVersion;
+	std::map<std::string, std::string> headers;
+	std::string body;
+} Request_t;
