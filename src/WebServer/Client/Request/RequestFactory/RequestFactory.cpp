e6b6538f9ce714bf5d71b3f236260aff9069b074
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-15 18:51:49 +0200

    feat: recognize_basic_HTTP_DELETE_request

--- src/WebServer/Client/Request/RequestFactory/RequestFactory.cpp
+++ src/WebServer/Client/Request/RequestFactory/RequestFactory.cpp
@@ -4,7 +4,11 @@ Result<RequestInfo_t> RequestFactory::create(const std::string &requestBuffer)
 {
     RequestInfo_t requestInfo;
     
-    (void) requestBuffer;
+    if (requestBuffer.find("GET") == 0)
+        requestInfo.request.method = GET;
+    else
+        requestInfo.request.method = DELETE;
+
     requestInfo.request.target = "/";
     requestInfo.request.httpVersion = "HTTP/1.1";
     requestInfo.request.headers["Host"] = "localhost";
--- tests/amethyst/suites/RequestFactory/RequestFactoryShould.cpp
+++ tests/amethyst/suites/RequestFactory/RequestFactoryShould.cpp
@@ -14,3 +14,16 @@ TEST(recognize_basic_HTTP_GET_request)
     ASSERT_EQUALS("localhost", request.headers.at("Host"));
     ASSERT_EQUALS("", request.body);
 }
+
+TEST(recognize_basic_HTTP_DELETE_request)
+{
+    Result<RequestInfo_t> actual = RequestFactory::create("DELETE / HTTP/1.1\r\nHost: localhost\r\n\r\n");
+    
+    Request_t request = actual.getValue().request;
+    ASSERT_EQUALS(DELETE, request.method);
+    ASSERT_EQUALS("/", request.target);
+    ASSERT_EQUALS("HTTP/1.1", request.httpVersion);
+    ASSERT_EQUALS(1, request.headers.size());
+    ASSERT_EQUALS("localhost", request.headers.at("Host"));
+    ASSERT_EQUALS("", request.body);
+}
