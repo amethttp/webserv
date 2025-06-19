10bb928bfd1a5bb4206bc0c77031e2150452c678
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-19 19:33:46 +0200

    refactor(test): extract createFromValidRequestLine method

--- tests/amethyst/suites/RequestParser/RequestParserShould.cpp
+++ tests/amethyst/suites/RequestParser/RequestParserShould.cpp
@@ -7,6 +7,16 @@ namespace
     RequestLineParams_t requestLineParams;
 }
 
+static RequestLineParams_t createFromValidRequestLine(const std::string &requestLineString)
+{
+    RequestTokenizer requestTokenizer(requestLineString);
+    RequestParser sut(requestTokenizer);
+
+    Result<RequestLineParams_t> result = sut.parseRequestLine();
+
+    return result.getValue();
+}
+
 static void assertRequestLine(method_t method, const std::string &target, const std::string &version)
 {
     ASSERT_EQUALS(method, requestLineParams.method);
@@ -16,33 +26,21 @@ static void assertRequestLine(method_t method, const std::string &target, const 
 
 TEST(should_recognize_a_basic_GET_request_line)
 {
-    RequestTokenizer requestTokenizer("GET / HTTP/1.1");
-    RequestParser sut(requestTokenizer);
-
-    Result<RequestLineParams_t> result = sut.parseRequestLine();
-    requestLineParams = result.getValue();
+    requestLineParams = createFromValidRequestLine("GET / HTTP/1.1");
 
     assertRequestLine(GET, "/", "HTTP/1.1");
 }
 
 TEST(should_recognize_a_basic_POST_request_line)
 {
-    RequestTokenizer requestTokenizer("POST / HTTP/1.1");
-    RequestParser sut(requestTokenizer);
-
-    Result<RequestLineParams_t> result = sut.parseRequestLine();
-    requestLineParams = result.getValue();
+    requestLineParams = createFromValidRequestLine("POST / HTTP/1.1");
 
     assertRequestLine(POST, "/", "HTTP/1.1");
 }
 
 TEST(should_recognize_a_basic_DELETE_request_line)
 {
-    RequestTokenizer requestTokenizer("DELETE / HTTP/1.1");
-    RequestParser sut(requestTokenizer);
-
-    Result<RequestLineParams_t> result = sut.parseRequestLine();
-    requestLineParams = result.getValue();
+    requestLineParams = createFromValidRequestLine("DELETE / HTTP/1.1");
 
     assertRequestLine(DELETE, "/", "HTTP/1.1");
 }
