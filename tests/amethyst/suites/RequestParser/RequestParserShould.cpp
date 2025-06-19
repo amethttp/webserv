797eac2b9806f0cf69ebb68efe4dfba8519783b4
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-19 19:16:20 +0200

    test: should_recognize_a_basic_DELETE_request_line

--- tests/amethyst/suites/RequestParser/RequestParserShould.cpp
+++ tests/amethyst/suites/RequestParser/RequestParserShould.cpp
@@ -27,3 +27,16 @@ TEST(should_recognize_a_basic_POST_request_line)
     ASSERT_EQUALS("/", requestLineParams.target);
     ASSERT_EQUALS("HTTP/1.1", requestLineParams.httpVersion);
 }
+
+TEST(should_recognize_a_basic_DELETE_request_line)
+{
+    RequestTokenizer requestTokenizer("DELETE / HTTP/1.1");
+    RequestParser sut(requestTokenizer);
+
+    Result<RequestLineParams_t> result = sut.parseRequestLine();
+    RequestLineParams_t requestLineParams = result.getValue();
+
+    ASSERT_EQUALS(DELETE, requestLineParams.method);
+    ASSERT_EQUALS("/", requestLineParams.target);
+    ASSERT_EQUALS("HTTP/1.1", requestLineParams.httpVersion);
+}
