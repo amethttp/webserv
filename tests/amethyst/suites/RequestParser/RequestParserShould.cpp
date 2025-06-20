1434d313ffc2310b2d859dd4ed1162dd2c20179d
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-20 17:22:15 +0200

    test: take_as_failure_a_request_line_with_multiple_first_SP

--- tests/amethyst/suites/RequestParser/RequestParserShould.cpp
+++ tests/amethyst/suites/RequestParser/RequestParserShould.cpp
@@ -119,6 +119,11 @@ TEST(take_as_failure_a_request_line_with_a_SP_and_HTAB)
     assertRequestLineIsInvalid("GET \t/ HTTP/1.1", "400 Bad Request");
 }
 
+TEST(take_as_failure_a_request_line_with_multiple_first_SP)
+{
+    assertRequestLineIsInvalid("GET   / HTTP/1.1", "400 Bad Request");
+}
+
 TEST(take_as_failure_a_request_line_preceded_by_SP)
 {
     assertRequestLineIsInvalid(" GET / HTTP/1.1", "400 Bad Request");
