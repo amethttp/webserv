ec9b22c55f1817d68bc55683aa4395f02536ab0a
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-22 12:16:59 +0200

    test: take_as_failure_a_HTAB_instead_of_HTTP_name

--- tests/amethyst/suites/RequestParser/RequestParserShould.cpp
+++ tests/amethyst/suites/RequestParser/RequestParserShould.cpp
@@ -177,6 +177,11 @@ TEST(take_as_failure_a_SP_instead_of_HTTP_name)
     assertRequestLineIsInvalid("GET /  /1.1", "400 Bad Request");
 }
 
+TEST(take_as_failure_a_HTAB_instead_of_HTTP_name)
+{
+    assertRequestLineIsInvalid("GET / \t/1.1", "400 Bad Request");
+}
+
 TEST(take_as_failure_a_request_line_preceded_by_SP)
 {
     assertRequestLineIsInvalid(" GET / HTTP/1.1", "400 Bad Request");
