18d3e2241a1d4adfbfc393c1df9ccf653c715bd2
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-22 13:08:19 +0200

    test: take_as_failure_an_empty_major_version

--- tests/amethyst/suites/RequestParser/RequestParserShould.cpp
+++ tests/amethyst/suites/RequestParser/RequestParserShould.cpp
@@ -247,6 +247,11 @@ TEST(take_as_failure_a_HTAB_instead_of_major_version)
     assertRequestLineIsInvalid("GET / HTTP/\t.1", "400 Bad Request");
 }
 
+TEST(take_as_failure_an_empty_major_version)
+{
+    assertRequestLineIsInvalid("GET / HTTP/.1", "400 Bad Request");
+}
+
 TEST(take_as_failure_a_request_line_preceded_by_SP)
 {
     assertRequestLineIsInvalid(" GET / HTTP/1.1", "400 Bad Request");
