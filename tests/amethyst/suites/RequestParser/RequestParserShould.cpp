6d95f1c12fc5c2d4205394c82042ec957e0eb9fd
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-25 19:58:52 +0200

    test: recognize_a_multiple_directory_target

--- tests/amethyst/suites/RequestParser/RequestParserShould.cpp
+++ tests/amethyst/suites/RequestParser/RequestParserShould.cpp
@@ -167,6 +167,13 @@ TEST(recognize_a_directory_target)
     assertRequestLine(GET, "/about/", "HTTP/1.1");
 }
 
+TEST(recognize_a_multiple_directory_target)
+{
+    requestLine = createFromValidRequestLine("GET /about/services/ HTTP/1.1");
+
+    assertRequestLine(GET, "/about/services/", "HTTP/1.1");
+}
+
 
 /* REQUEST LINE LAST SP CRITERIA */
 TEST(take_as_failure_a_request_line_without_the_last_SP)
