2e6d214870546fc243222ae188397e9fb72fc917
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-26 16:03:40 +0200

    test: recognize_a_query_with_multiple_parameters

--- tests/amethyst/suites/RequestParser/RequestParserShould.cpp
+++ tests/amethyst/suites/RequestParser/RequestParserShould.cpp
@@ -249,6 +249,13 @@ TEST(recognize_a_query_with_a_full_parameter)
     assertRequestLine(GET, "/VALID/PATH/?param=anyValue", "HTTP/1.1");
 }
 
+TEST(recognize_a_query_with_multiple_parameters)
+{
+    requestLine = createFromValidRequestLine("GET /VALID/PATH/?param=anyValue&mode= HTTP/1.1");
+
+    assertRequestLine(GET, "/VALID/PATH/?param=anyValue&mode=", "HTTP/1.1");
+}
+
 
 /* REQUEST LINE LAST SP CRITERIA */
 TEST(take_as_failure_a_request_line_without_the_last_SP)
