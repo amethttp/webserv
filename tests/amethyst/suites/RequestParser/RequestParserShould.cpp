fd48cb6a1922d25db7a108b2e5aba064eadee9ae
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-26 18:19:41 +0200

    test: recognize_a_query_with_multiple_ampersands

--- tests/amethyst/suites/RequestParser/RequestParserShould.cpp
+++ tests/amethyst/suites/RequestParser/RequestParserShould.cpp
@@ -353,6 +353,13 @@ TEST(recognize_a_query_with_a_single_ampersand)
     assertRequestLine(GET, "/?&", "HTTP/1.1");
 }
 
+TEST(recognize_a_query_with_multiple_ampersands)
+{
+    requestLine = createFromValidRequestLine("GET /?&&&&&& HTTP/1.1");
+
+    assertRequestLine(GET, "/?&&&&&&", "HTTP/1.1");
+}
+
 
 /* REQUEST LINE LAST SP CRITERIA */
 TEST(take_as_failure_a_request_line_without_the_last_SP)
