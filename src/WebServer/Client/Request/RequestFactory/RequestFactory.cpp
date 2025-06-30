a99741e04b12a56d5df9c9ea5fbf3a96c275157c
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-06-30 14:59:57 +0200

    refactor: extract uriBegin and uriEnd local variables

--- src/WebServer/Client/Request/RequestFactory/RequestFactory.cpp
+++ src/WebServer/Client/Request/RequestFactory/RequestFactory.cpp
@@ -9,11 +9,13 @@
 
 void RequestFactory::splitRequestTargetComponents(Target_t &target)
 {
-    const std::string::iterator queryStart = std::find(target.uri.begin(), target.uri.end(), '?');
+    const std::string::iterator uriBegin = target.uri.begin();
+    const std::string::iterator uriEnd = target.uri.end();
+    const std::string::iterator queryStart = std::find(uriBegin, uriEnd, '?');
 
-    target.path = std::string(target.uri.begin(), queryStart);
-    if (queryStart != target.uri.end())
-        target.query = std::string(queryStart + 1, target.uri.end());
+    target.path = std::string(uriBegin, queryStart);
+    if (queryStart != uriEnd)
+        target.query = std::string(queryStart + 1, uriEnd);
 }
 
 Result<std::string> RequestFactory::decodeTarget(const std::string &encodedTarget)
