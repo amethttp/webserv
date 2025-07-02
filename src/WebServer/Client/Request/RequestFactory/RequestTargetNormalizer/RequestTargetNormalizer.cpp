c06bde184debacb1e9a8457c9733db519a43a0ea
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-07-02 15:20:06 +0200

    refactor: extract buildNewPath method

--- src/WebServer/Client/Request/RequestFactory/RequestTargetNormalizer/RequestTargetNormalizer.cpp
+++ src/WebServer/Client/Request/RequestFactory/RequestTargetNormalizer/RequestTargetNormalizer.cpp
@@ -15,11 +15,24 @@ std::vector<std::string> RequestTargetNormalizer::normalizePathComponents(const 
     {
         if (*it == ".")
             continue;
-
         processedPathComponents.push_back(*it);
     }
 
     return processedPathComponents;
+}
+
+std::string RequestTargetNormalizer::buildNewPath(const std::vector<std::string> &newPathComponents)
+{
+    std::string newPath = "/";
+
+    for (std::vector<std::string>::const_iterator it = newPathComponents.begin(); it != newPathComponents.end(); ++it)
+    {
+        newPath += *it;
+        if (it != newPathComponents.end() - 1)
+            newPath += "/";
+    }
+
+    return newPath;
 }
 
 void RequestTargetNormalizer::normalizePath(std::string &path)
@@ -28,17 +41,6 @@ void RequestTargetNormalizer::normalizePath(std::string &path)
         path += '/';
 
     const std::vector<std::string> pathComponents = split(path, "/");
-    std::vector<std::string> newPathComponents = normalizePathComponents(pathComponents);
-
-
-    std::string newPath = "/";
-    for (std::vector<std::string>::iterator it = newPathComponents.begin(); it != newPathComponents.end(); ++it)
-    {
-        newPath += *it;
-
-        if (it != newPathComponents.end() - 1)
-            newPath += "/";
-    }
-
-    path = newPath;
+    const std::vector<std::string> newPathComponents = normalizePathComponents(pathComponents);
+    path = buildNewPath(newPathComponents);
 }
--- src/WebServer/Client/Request/RequestFactory/RequestTargetNormalizer/RequestTargetNormalizer.hpp
+++ src/WebServer/Client/Request/RequestFactory/RequestTargetNormalizer/RequestTargetNormalizer.hpp
@@ -9,7 +9,9 @@ private:
 
     static bool hasTrailingDotSegment(const std::string &path);
     static std::vector<std::string> normalizePathComponents(const std::vector<std::string> &pathComponents);
+    static std::string buildNewPath(const std::vector<std::string> &newPathComponents);
 
 public:
+
     static void normalizePath(std::string &path);
 };
