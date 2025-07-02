f9f34ea585a72d6859410dc0e783f83be48c347d
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-07-02 21:16:34 +0200

    test: normalize_a_multiple_slashes_path_after_text

--- tests/amethyst/suites/RequestFactory/RequestFactoryShould.cpp
+++ tests/amethyst/suites/RequestFactory/RequestFactoryShould.cpp
@@ -550,6 +550,14 @@ TEST(normalize_a_multiple_slashes_path)
     assertRequestLine(GET, "////", "HTTP/1.1");
 }
 
+TEST(normalize_a_multiple_slashes_path_after_text)
+{
+    request = createRequestFromValidRequestLine("GET /courses////// HTTP/1.1");
+
+    assertTargetComponents("/courses/", "");
+    assertRequestLine(GET, "/courses//////", "HTTP/1.1");
+}
+
 
 /* REQUEST HEADERS TESTS */
 TEST(recognize_basic_HTTP_request_without_OWS_inside_headers)
