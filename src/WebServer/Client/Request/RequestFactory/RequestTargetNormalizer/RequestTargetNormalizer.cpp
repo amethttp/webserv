369ade22af925c82fc15d59e0b588f36f09a7767
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-07-02 13:45:09 +0200

    feat: normalize_a_single_current_directory_after_text

--- src/WebServer/Client/Request/RequestFactory/RequestTargetNormalizer/RequestTargetNormalizer.cpp
+++ src/WebServer/Client/Request/RequestFactory/RequestTargetNormalizer/RequestTargetNormalizer.cpp
@@ -8,9 +8,25 @@ void RequestTargetNormalizer::normalizePath(std::string &path)
         currentPathPos = path.find("/.");
 
         if (currentPathPos != std::string::npos)
-            path.replace(currentPathPos, 2, "");
+            path.replace(currentPathPos, 2, "/");
     } while (currentPathPos != std::string::npos);
 
     if (path.empty())
         path = "/";
+
+    bool itsSlash = false;
+    for (size_t i = 0; i < path.size(); i++)
+    {
+        if (path[i] == '/')
+        {
+            if (itsSlash)
+            {
+                path.replace(i, 1, "");
+                i--;
+            }
+            itsSlash = true;
+        }
+        else
+            itsSlash = false;
+    }
 }
