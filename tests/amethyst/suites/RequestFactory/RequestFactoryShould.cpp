3e3d1e071006335b5dd039c823f3fa90c92102a0
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-07-02 20:33:22 +0200

    test: normalize_a_multiple_parent_directory_after_text_ended_with_slash

--- tests/amethyst/suites/RequestFactory/RequestFactoryShould.cpp
+++ tests/amethyst/suites/RequestFactory/RequestFactoryShould.cpp
@@ -470,6 +470,14 @@ TEST(normalize_a_multiple_parent_directory_after_text)
     assertRequestLine(GET, "/courses/../../..", "HTTP/1.1");
 }
 
+TEST(normalize_a_multiple_parent_directory_after_text_ended_with_slash)
+{
+    request = createRequestFromValidRequestLine("GET /courses/../../../ HTTP/1.1");
+
+    assertTargetComponents("/", "");
+    assertRequestLine(GET, "/courses/../../../", "HTTP/1.1");
+}
+
 
 /* REQUEST HEADERS TESTS */
 TEST(recognize_basic_HTTP_request_without_OWS_inside_headers)
