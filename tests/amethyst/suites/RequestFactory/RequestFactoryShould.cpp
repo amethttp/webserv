3719f205717c2d272bfce2c7cafb2bfec02c6bf6
Christian Fidalgo Areste <chrisfidare@yahoo.com>
2025-07-02 14:47:00 +0200

    test: normalize_a_single_current_directory_after_text_ended_with_slash

--- tests/amethyst/suites/RequestFactory/RequestFactoryShould.cpp
+++ tests/amethyst/suites/RequestFactory/RequestFactoryShould.cpp
@@ -334,6 +334,14 @@ TEST(normalize_a_single_current_directory_after_text)
     assertRequestLine(GET, "/courses/.", "HTTP/1.1");
 }
 
+TEST(normalize_a_single_current_directory_after_text_ended_with_slash)
+{
+    request = createRequestFromValidRequestLine("GET /courses/./ HTTP/1.1");
+
+    assertTargetComponents("/courses/", "");
+    assertRequestLine(GET, "/courses/./", "HTTP/1.1");
+}
+
 
 /* REQUEST HEADERS TESTS */
 TEST(recognize_basic_HTTP_request_without_OWS_inside_headers)
