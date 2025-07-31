07767be9d74922c0d2d6b5def76c9c8a03bf164c
arzelcm <arzelcanavate@gmail.com>
2025-07-31 14:49:11 +0200

    refactor(parser): delete unnecessary code sections

--- src/WebServer/ConfigParser/ConfigParser.cpp
+++ src/WebServer/ConfigParser/ConfigParser.cpp
@@ -37,7 +37,6 @@ static std::vector<Token> tokenize(const std::string &input)
 	std::vector<Token> tokens;
 	std::istringstream stream(input);
 	std::string line;
-
 	while (std::getline(stream, line))
 	{
 		std::string token;
@@ -45,7 +44,6 @@ static std::vector<Token> tokenize(const std::string &input)
 		for (size_t i = 0; i < line.size(); ++i)
 		{
 			char c = line[i];
-
 			if (in_comment)
 				break;
 			if (c == '#')
@@ -90,83 +88,29 @@ static std::vector<Token> tokenize(const std::string &input)
 				tokens.push_back((Token){TOKEN_SEMICOLON, ";"});
 			}
 			else
-			{
 				token += c;
-			}
 		}
 		if (!token.empty())
-		{
 			tokens.push_back((Token){TOKEN_WORD, token});
-		}
 	}
-
 	return tokens;
 }
 
-void print_tree(const std::vector<ConfigNode> &nodes, int indent = 0)
-{
-	for (size_t i = 0; i < nodes.size(); ++i)
-	{
-		for (int j = 0; j < indent; ++j)
-			std::cout << "  ";
-		std::cout << nodes[i].name;
-		for (size_t k = 0; k < nodes[i].params.size(); ++k)
-			std::cout << " " << nodes[i].params[k];
-		std::cout << "\n";
-		print_tree(nodes[i].children, indent + 1);
-	}
-}
-
-void print_tokens(const std::vector<Token> &tokens)
-{
-	for (size_t i = 0; i < tokens.size(); ++i)
-	{
-		const Token &t = tokens[i];
-		std::string type;
-		switch (t.type)
-		{
-		case TOKEN_WORD:
-			type = "WORD";
-			break;
-		case TOKEN_LBRACE:
-			type = "LBRACE";
-			break;
-		case TOKEN_RBRACE:
-			type = "RBRACE";
-			break;
-		case TOKEN_SEMICOLON:
-			type = "SEMICOLON";
-			break;
-		default:
-			type = "UNKNOWN";
-			break;
-		}
-		std::cout << "[" << type << "] \"" << t.value << "\"" << std::endl;
-	}
-}
-
-static void parse_config(const std::string &input, std::vector<ConfigNode> &config_tree)
+static void parseConfig(const std::string &input, std::vector<ConfigNode> &config_tree)
 {
 	std::vector<Token> tokens = tokenize(input);
-	// print_tokens(tokens);
 	std::vector<Token>::const_iterator it = tokens.begin();
 	std::vector<Token>::const_iterator end = tokens.end();
-
 	std::stack<Frame> stack;
 	stack.push((Frame){&config_tree, ConfigNode(), false});
-
 	while (it != end)
 	{
 		Token tok = *it++;
-
 		if (tok.type == TOKEN_WORD)
 		{
-			// std::cout << "ENTRY: " << stack.top().current.name << std::endl;
 			Frame &frame = stack.top();
-
 			if (!frame.building)
 			{
-				// std::cout << "Building " << tok.value << std::endl;
 				frame.current.name = tok.value;
 				frame.current.params.clear();
 				frame.current.children.clear();
@@ -187,7 +131,6 @@ static void parse_config(const std::string &input, std::vector<ConfigNode> &conf
 			stack.top().container->push_back(stack.top().current);
 			stack.top().current = ConfigNode();
 			stack.top().building = false;
-			// std::cout << "SEMICOLON" << " ( " << stack.top().current.name << " ) " << &stack.top() << std::endl;
 		}
 		else if (tok.type == TOKEN_LBRACE)
 		{
@@ -205,7 +148,6 @@ static void parse_config(const std::string &input, std::vector<ConfigNode> &conf
 		}
 		else if (tok.type == TOKEN_RBRACE)
 		{
-			// std::cout << "RBRACE " << &stack.top() << std::endl;
 			if (stack.size() <= 1)
 			{
 				std::stringstream what;
@@ -290,7 +232,6 @@ static void fillBlockFromNode(const ConfigNode &node, ConfigBlock &block)
 
 static void mapTreeToBlock(const std::vector<ConfigNode> &tree, ConfigBlock &block)
 {
-	// std::cout << "Entro once" << std::endl;
 	std::vector<ConfigNode> pendingChildren;
 	for (std::vector<ConfigNode>::const_iterator it = tree.begin(); it != tree.end(); ++it)
 	{
@@ -300,10 +241,8 @@ static void mapTreeToBlock(const std::vector<ConfigNode> &tree, ConfigBlock &blo
 		else
 			fillBlockFromNode(node, block);
 	}
-	// std::cout << block << std::endl;
 	for (std::vector<ConfigNode>::iterator it = pendingChildren.begin(); it != pendingChildren.end(); ++it)
 	{
-		// std::cout << "Pending size" << pendingChildren.size() << std::endl;
 		block.children.push_back(block);
 		if (it->name == "server")
 			block.children.back().type = SERVER_BLOCK;
@@ -382,7 +321,6 @@ static void fillWebserver(WebServer &webServer, std::vector<ConfigNode> &tree)
 
 void ConfigParser::useConfig(std::string &path, WebServer &ws)
 {
-	std::cout << "parsing " << path << std::endl;
 	t_configs configs;
 	configs.path = &path;
 	configs.ws = &ws;
@@ -390,7 +328,7 @@ void ConfigParser::useConfig(std::string &path, WebServer &ws)
 	configs.fileContent = readFileToString(path);
 	std::vector<ConfigNode> tree;
 	checkFileAcceptance(configs);
-	parse_config(configs.fileContent, tree);
+	parseConfig(configs.fileContent, tree);
 	fillWebserver(ws, tree);
 	std::cout << ws << std::endl;
 }
